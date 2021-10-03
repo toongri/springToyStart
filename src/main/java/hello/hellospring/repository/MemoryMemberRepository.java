3527e55d5fb173f16d4a6907c492c71cf62f19bd
Toong_laptop <jhkim03284@gmail.com>
2021-10-03 22:00:31 +0900

    3th commit : project 구현 시도 중

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.swing.text.html.Option;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
