2a2f9db494784dc8425627382b6c724128dadb99
Toong_laptop <jhkim03284@gmail.com>
2021-10-05 21:19:30 +0900

    5th commit : 클래스들 스프링 빈 등록

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,6 +2,8 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +11,6 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemoryMemberRepository memberRepository;
-
     public MemberService(MemoryMemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
